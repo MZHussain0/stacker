d9010ab75359653b7a71760c34726c96bc02b11d
“mzhussain0” <mzhussain092@gmail.com>
2023-09-24 15:26:07 +0530

    add stripe integeration

---
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -0,0 +1,96 @@
+﻿import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const user = await currentUser();
+
+    if (!user || !user.id || !user.emailAddresses?.[0]?.emailAddress) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    const purchase = await db.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          courseId: params.courseId,
+          userId: user.id,
+        },
+      },
+    });
+
+    if (purchase) {
+      return NextResponse.json(purchase, { status: 400 });
+    }
+
+    if (!course) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: course.title,
+            description: course.description!,
+          },
+          unit_amount: Math.round(course.price! * 100),
+        },
+      },
+    ];
+
+    let stripeCustomer = await db.stripeCustomer.findUnique({
+      where: {
+        userId: user.id,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
+
+    if (!stripeCustomer) {
+      const customer = await stripe.customers.create({
+        email: user.emailAddresses?.[0]?.emailAddress!,
+      });
+
+      stripeCustomer = await db.stripeCustomer.create({
+        data: {
+          userId: user.id,
+          stripeCustomerId: customer.id,
+        },
+      });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items,
+      mode: "payment",
+      customer: stripeCustomer.stripeCustomerId,
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?status=success`,
+      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?status=cancelled`,
+      metadata: {
+        courseId: course.id,
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json({ url: session.url }, { status: 200 });
+  } catch (error) {
+    console.log("[CHECKOUT_ID:POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
